e486a4c31277ddce37cac822ece02270304546e7
BaconEater314 <nickbeaty314@gmail.com>
2025-08-14 18:34:48 -0600

    Json stuff 8/14/2025

--- Source/Game/Main.cpp
+++ Source/Game/Main.cpp
@@ -7,6 +7,44 @@ int main(int argc, char* argv[]) {
 
     file::SetCurrentDirectory("Assets");
 
+    // load the json data from a file
+    std::string buffer;
+    file::ReadTextFile("Ajson.txt", buffer);
+    // show the contents of the json file (debug)
+    std::cout << buffer << std::endl;
+
+    // create json document from the json file contents
+    rapidjson::Document document;
+    json::Load("json.txt", document);
+
+    // read the age data from the json
+    int age;
+    json::Read(document, "age", age);
+    // show the age data
+    std::cout << age << std::endl;
+
+    // read/show the data from the json file
+    std::string name;
+    float speed;
+    bool isAwake;
+    vec2 position;
+    vec3 color;
+
+    // read the json data
+    JSON_READ(document, name);
+    JSON_READ(document, age);
+    JSON_READ(document, speed);
+    JSON_READ(document, isAwake);
+    JSON_READ(document, position);
+    JSON_READ(document, color);
+
+    // show the data
+    std::cout << name << " " << age << " " << speed << " " << isAwake << std::endl;
+    std::cout << position.x << " " << position.y << std::endl;
+    std::cout << color.r << " " << color.g << " " << color.b << " " << std::endl;
+
+    return 0;
+
     //initialize Engine
     Logger::Info("Initialize Engine");
     GetEngine().Initialize();
